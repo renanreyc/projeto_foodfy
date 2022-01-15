c158337b0d3778d111b904f04c41b125df938d6e
Renan Rey <renan.reyc@gmail.com>
2022-01-15 14:07:34 -0300

    projeto final 01: organizacao das pastas, banco de dados, src e public

---
+++ public/scripts/hideRecipeContent.js
@@ -0,0 +1,14 @@
+const hideContent = document.querySelectorAll('.recipe-hide');
+
+for (let item of hideContent) {
+    const hideButton = item.querySelector('.hide');
+
+    hideButton.addEventListener('click', () => {
+        item.querySelector('.content').classList.toggle('hidden');
+        if (hideButton.innerHTML == 'ESCONDER') {
+            hideButton.innerHTML = 'MOSTRAR';
+        } else {
+            hideButton.innerHTML = 'ESCONDER';
+        }
+    });
+}
